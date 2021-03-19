f2f323b22184a73c38464bd188a5ba233ee88f68
yetra <brdujmovic@gmail.com>
2021-03-20 00:13:55 +0100

    Rename variables

--- lab01/SimHashBuckets.py
+++ lab01/SimHashBuckets.py
@@ -1,31 +1,31 @@
 import numpy as np
 
 
-def lsh(hashes, num_bands=8):
-    band_size = len(hashes[0]) / num_bands
+def lsh(text_hashes, num_bands=8):
+    band_size = len(text_hashes[0]) / num_bands
     powers_of_2 = 2 ** np.arange(band_size)[::-1]
 
-    candidates = {i: set() for i in range(len(hashes))}
+    candidates = {i: set() for i in range(len(text_hashes))}
 
     for band in range(num_bands):
         buckets = {}
 
-        for i, text_hash in enumerate(hashes):
-            band_start, band_end = band * band_size, (band + 1) * band_size
-            band_bits = text_hash[band_start:band_end]
+        for text_idx, text_hash in enumerate(text_hashes):
+            start, end = band * band_size, (band + 1) * band_size
+            band_bits = text_hash[start:end]
             band_value = band_bits.dot(powers_of_2)
 
             if band_value in buckets:
-                texts_in_bucket = buckets[band_value]
+                bucket = buckets[band_value]
 
-                for text_i in texts_in_bucket:
-                    candidates[i].add(text_i)
-                    candidates[text_i].add(i)
+                for idx in bucket:
+                    candidates[text_idx].add(idx)
+                    candidates[idx].add(text_idx)
 
             else:
-                texts_in_bucket = set()
+                bucket = set()
 
-            texts_in_bucket.add(i)
-            buckets[band_value] = texts_in_bucket
+            bucket.add(text_idx)
+            buckets[band_value] = bucket
 
     return candidates
