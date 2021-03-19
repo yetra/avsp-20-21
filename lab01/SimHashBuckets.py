ebff89f2143f0710a18ec307a4dc1ecbb9399a5b
yetra <brdujmovic@gmail.com>
2021-03-20 00:26:23 +0100

    Add function for LSH-based similar document search

--- lab01/SimHashBuckets.py
+++ lab01/SimHashBuckets.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+from SimHash import simhash
+
 
 def lsh(text_hashes, num_bands=8):
     """
@@ -33,3 +35,46 @@ def lsh(text_hashes, num_bands=8):
             buckets[band_value] = bucket
 
     return candidates
+
+
+def lsh_search(file_path):
+    """
+    Performs a search of similar texts based on queries
+    specified in the given file and LSH similarity candidates.
+
+    Similar files are identified using the SimHash algorithm.
+
+    This function expects the file to be in the following format:
+    * the first line contains the number of texts to read - N
+    * the next N lines are the N texts with space-separated tokens
+    * the (N+1)-th line contains the number of queries to perform - Q
+    * the next Q lines are the Q queries of the form - I K
+      - output the number of texts whose hashes differ from
+        the hash of the I-th text by at most K bits
+
+    :param file_path: the path of the file containing texts and queries
+    """
+    text_hashes = []
+
+    with open(file_path) as file:
+        num_texts = int(next(file).strip())
+
+        for _ in range(num_texts):
+            line = next(file).strip()
+            text_hashes.append(simhash(line))
+
+        candidates = lsh(text_hashes)
+
+        num_queries = int(next(file).strip())
+
+        for _ in range(num_queries):
+            i, k = map(int, next(file).strip().split())
+            ith_candidates = candidates.get(i, set())
+
+            num_diff_texts = 0
+
+            for text_idx in ith_candidates:
+                if (text_hashes[i] != text_hashes[text_idx]).sum() <= k:
+                    num_diff_texts += 1
+
+            print(num_diff_texts)
