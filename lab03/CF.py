a188bff241677acf1514423be5133106f7530bcc
yetra <brdujmovic@gmail.com>
2021-04-18 00:07:02 +0200

    Add CollaborativeFiltering class

--- lab03/CF.py
+++ lab03/CF.py
@@ -3,6 +3,23 @@ import sys
 import numpy as np
 
 
+class CollaborativeFiltering:
+    """Class for item-item and user-user collaborative filtering."""
+
+    def __init__(self, ratings, num_items, num_users):
+        """
+        Inits the CollaborativeFiltering class.
+
+        :param ratings: the num_items x num_users ratings matrix
+        :param num_items: the total number of items
+        :param num_users: the total number of users
+        """
+        self.ratings = ratings
+
+        self.num_items = num_items
+        self.num_users = num_users
+
+
 if __name__ == '__main__':
     num_items, num_users = map(int, sys.stdin.readline().rstrip().split())
 
