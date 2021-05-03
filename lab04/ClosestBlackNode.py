f76cc7d7d296a60cde4c671e8be83ededf0cc46c
yetra <brdujmovic@gmail.com>
2021-05-03 22:03:24 +0200

    Add function for parsing edges

--- lab04/ClosestBlackNode.py
+++ lab04/ClosestBlackNode.py
@@ -1,4 +1,5 @@
 import sys
+from collections import defaultdict
 
 
 def parse_node_colors(num_nodes):
@@ -20,3 +21,24 @@ def parse_node_colors(num_nodes):
             black_nodes.add(node)
 
     return black_nodes
+
+
+def parse_edges(num_edges):
+    """
+    Parses graph edges from sys.stdin.
+
+    The function expects num_edges line where each line contains two node
+    indices separated by a whitespace.
+
+    :param num_edges: the number of edges in the graph
+    :return: a dict representing the graph's adjacency matrix
+    """
+    adj_matrix = defaultdict(list)
+
+    for _ in range(num_edges):
+        node_1, node_2 = map(int, sys.stdin.readline().rstrip().split())
+
+        adj_matrix[node_1].append(node_2)
+        adj_matrix[node_2].append(node_1)
+
+    return adj_matrix
