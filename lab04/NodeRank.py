e3f788d23a81aa40d929b0a9ceef436af7f77014
yetra <brdujmovic@gmail.com>
2021-05-03 14:49:54 +0200

    Move reading num_nodes & beta to main program

--- lab04/NodeRank.py
+++ lab04/NodeRank.py
@@ -2,19 +2,15 @@ import sys
 from scipy.sparse import csr_matrix
 
 
-def parse_graph():
+def parse_graph(num_nodes):
     """
     Parses input from sys.stdin into a sparse adjacency matrix.
 
-    The function expects input of the following form:
-    * the first line contains the number of nodes and the beta probability
-    * each row i of the following num_nodes rows contains the indices of nodes
-      adjacent to i
-    :return: the number of nodes in the graph, the beta probability value,
-             and the parsed adjacency matrix in scipy.sparse.crs_matrix form
-    """
-    num_nodes, beta = map(int, sys.stdin.readline().rstrip().split())
+    The function expects num_nodes lines such that line i contains the indices
+    of nodes adjacent to i.
 
+    :return: the parsed adjacency matrix in scipy.sparse.crs_matrix form
+    """
     indptr = [0]
     indices = []
     data = []
@@ -26,6 +22,8 @@ def parse_graph():
         data += [1] * len(adj_nodes)
         indptr.append(len(indices))
 
-    adj_matrix = csr_matrix((data, indices, indptr), dtype=int)
+    return csr_matrix((data, indices, indptr), dtype=int)
 
-    return num_nodes, beta, adj_matrix
+
+if __name__ == '__main__':
+    num_nodes, beta = map(int, sys.stdin.readline().rstrip().split())
